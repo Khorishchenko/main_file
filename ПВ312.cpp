e357e5271b2fe7a992262ff137368f1bb9b7a0e9
admin <admin@MacBook-Air-admin.local>
2024-06-21 20:30:40 +0300

    new file

--- ПВ312.cpp
+++ ПВ312.cpp
@@ -33,260 +33,157 @@
 
 // https://www.youtube.com/watch?v=1cKvMZOJeeE&ab_channel=%23SimpleCode - VECTOR
 
+#include <map>
+#include <iterator>
+using namespace std;
 
-// int main() 
-// {
-//     std::vector<int> vec = {1, 2, 3, 4, 5};
-//     std::vector<int> vec2;
-//     std::vector<int> vec3(10);
+// https://codelessons.ru/cplusplus/map-v-c-chto-eto-i-kak-s-etim-rabotat.html --- MAP
+#include <iostream>
+#include <map>
+#include <string>
 
+int main() {
+    // Як створити map
+    // map <L, R> <ім'я>;
+    // <L> — цей тип даних буде відноситись до значення ключа.
+    // <R> — цей тип даних відноситься до значення.
 
-    // std::cout << "Elements of the vector in reverse order: ";
-    // for ( std::vector<int>::reverse_iterator rit = vec.rbegin(); rit != vec.rend(); ++rit ) {
-    //     std::cout << *rit << " ";
-    // }
-    // std::cout << std::endl;
+    // Приклад 1: map з ключами типу int і значеннями типу int
+    std::map < int, int > mp1;
 
-    // auto it = vec.begin();
+    mp1[ 0 ] = 7; // ключ - число
+    std::cout << "mp1[0] = " << mp1[0] << std::endl;
 
-    // for (; it != vec.end(); it++ )
-    //     std::cout << *it << ' ';
-    // std::cout << std::endl;
+    // Приклад 2: map з ключами типу string і значеннями типу int
+    std::map<std::string, int> mp2;
+    mp2["zero"] = 4;  // ключ - рядок
+    mp2["zero"] = 10;
+    std::cout << "mp2[\"zero\"] = " << mp2["zero"] << std::endl;
 
+    // Приклад 3: map з ініціалізацією елементів
 
-    // for ( int i = vec.size() - 1; i >= 0 ; i-- )
-    //     std::cout << vec[i] << ' ';
-    // std::cout << std::endl;
-    
 
 
-    // std::list<int> lst {10, 20, 30, 40, 50};
 
-    // std::list<int>::iterator it = lst.begin();
+    // std::map<std::string, std::string> book {
+    //     {"Hi", "Привет"},
+    //     {"Student", "Студент"},
+    //     {"!", "!"}
+    // };
 
-    // while ( it != lst.end() )
-    // {
-    //     std::cout << *it << ' ';
-    //     it++;
-    // }
-    // std::cout << std::endl;
+    // std::cout << "book[\"Hi\"] = " << book["Hi"] << std::endl;
+    // book["Hi"] = "Hello";                       
+    // std::cout << "book[\"Hi\"] = " << book["Hi"] << std::endl;
 
-    // it = lst.begin();
 
 
 
-    // while ( it != lst.end() )
-    // {
-    //     if ( *it == 30 )
-    //         lst.erase(it);
-    //     it++;
-    // }
-    // std::cout << std::endl;
 
 
 
-    // for (const auto& element : lst )
-    //     std::cout << element << ' ';
-    // std::cout << std::endl;
-    
-    // int index;
-    // std::cout << "enter index element: ";
-    // std::cin >> index;
 
-    // it = lst.begin();
-    // advance(it, --index);
-    // lst.erase(it);
 
-    // for (const auto& element : lst )
-    //     std::cout << element << ' ';
-    // std::cout << std::endl;
+    // Використання ітераторів для map
+    // map <тип даних>::iterator <ім'я>;
 
+    // Створення map для прикладу з ітераторами
 
-    // lst.push_back(22);
 
 
-    // for (const auto& element : lst )
-    //     std::cout << element << ' ';
-    // std::cout << std::endl;
-
-
-    // std::cout << "enter index element: ";
-    // std::cin >> index;
-
-    // int data;
-    // std::cout << "enter element: ";
-    // std::cin >> data;
-
-
-    // it = lst.begin();
-    // advance(it, --index);
-
-    // lst.insert(it, data);
-
-
-    // for (const auto& element : lst )
-    //     std::cout << element << ' ';
-    // std::cout << std::endl;
-
-
-    // lst.sort();
-
-    // for (const auto& element : lst )
-    //     std::cout << element << ' ';
-    // std::cout << std::endl;
-
-
-    // lst.push_front(33);
-
-
-    // for (const auto& element : lst )
-    //     std::cout << element << ' ';
-    // std::cout << std::endl;
-
-
-
-//     return 0;
-// }
-
-
-
-// using namespace std;
-
-bool is_anagram(std::string word, std::string anagram);
-int count_unique_words(std::string sentence);
-
-
-int main()
-{
-
-
-	// std::set <int> st; // пример
-    
-// Чтобы добавить новый элемент нужно использовать функцию insert():
-
-	// for (int i = 5; i >= 0; i--){
-	// 	st.insert(i); // 5 4 3 2 1 0
-	// }
-
-	// std::set<int>::iterator it = st.begin();
-
-	// while (it != st.end()) 
-	// {
-	// 	std::cout << *it++ << ' ';
-	// }
-    // cout << endl;
-
-
-// add new element value = 4;
-
-	// std::cout << "New add element, value == 4" << std::endl;
-	// st.insert(4);
-
-	// copy(st.begin(), st.end(), std::ostream_iterator<int>(std::cout, " ")), std::cout << '\n';
-
-// изменить сторону сортировки в обратную
-// std::cout << "new set rever element !!!" << std::endl;
-
-
-	// std::set <int, std::greater <int> > st2; 
-	// for (int i = 0; i < 5; i++){
-	// 	st2.insert(i); // 0 1 2 3 4
-	// }
-
-	// std::set <int> :: iterator it2 = st2.begin();
-
-	// copy(st2.begin(), st2.end(), std::ostream_iterator<int>(std::cout, " ")), std::cout << '\n';
-
-
-// multiset 
-
-	// std::multiset <int> mst;
+    std::map<int, int> mp;
+    // std::cout << "Введіть кількість елементів: "; 
+    // int n; 
+    // std::cin >> n;
  
-    // std::cout << "Добавление случайных значений: " << std::endl;
-    // for (int i = 0; i < 10; i++) {
-    //     int random = rand() % 10 + 1;
-    //     mst.insert(random);
-    //     std::cout << i + 1 << ") " << random << std::endl;
+    // for (int i = 0; i < n; i++) {
+    //     std::cout << "Елемент " << i + 1 << ": "; 
+    //     int a, b; 
+    //     std::cin >> a >> b;
+    //     mp[a] = b;  // додаємо нові елементи
     // }
  
-    // std::multiset <int> :: iterator it = mst.begin();
- 
-    // std::cout << "Отсортированный вариант: " << std::endl;
-    // for (int i = 1; it != mst.end(); i++, it++) {
-    //     std::cout << *it << " ";
-    // }
-    // std::cout << "\n\n";
+    // std::map<int, int>::iterator it = mp.begin();
 
- // delete elemet
-    // std::cout << "Delete element: \n";
-    // mst.erase(4);
+    // // auto it_auto = mp.begin();
+    // // auto value = 10;
 
-	// copy(mst.begin(), mst.end(), std::ostream_iterator<int>(std::cout, " ")), std::cout << '\n';
-
-// lower_bound
-	// std::cout << "lower_bound: = ";
-	// it = mst.lower_bound(2);
-	// std::cout << *it << std::endl;
-
-
-// count
-	// std::cout << mst.count(10) << std::endl;
-	// std::cout << mst.size() << std::endl;
-
-
-
-
-// Вивести всі унікальні елементи з масиву arr:
-
-   	// int arr[] = {1, 2, 3, 4, 1, 2, 5};
-    // std::set<int> unique_elems;
-
-    // for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
-    //     unique_elems.insert(arr[i]);
+    // std::cout << "Всі елементи map, відсортовані за ключем: " << std::endl;
+    // for (int i = 0; it != mp.end(); ++it, ++i) {  // виводимо їх
+    //     std::cout << i << ") Ключ " << it->first << ", значення " << it->second << std::endl;
     // }
 
-    // for (int elem : unique_elems) {
-    //     std::cout << elem << " ";
-    // }
-    // std::cout << std::endl;
+    // for ( const auto& element : mp )
+    //     std::cout <<  " Ключ " << element.first << ", значення " << element.second << std::endl;
 
-// Перевірити, чи є дане слово word анаграмою іншого слова anagram:
-// Анаграма - це слово або фраза, створена шляхом перестановки літер іншого слова або фрази. Наприклад, анаграмою слова "кінь" може бути слово "нік".
 
-	// std::string word = "listen";
-    // std::string anagram = "silent";
+    // Приклад використання insert
+    int key = 0, value = 5;
+    mp[1] = 11;
+    mp.insert(std::make_pair(key, value));
 
-    // if (is_anagram(word, anagram)) {
-    //     std::cout << word << " and " << anagram << " are anagrams" << std::endl;
-    // } else {
-    //     std::cout << word << " and " << anagram << " are not anagrams" << std::endl;
-    // }
-
-// Визначити кількість унікальних слів у рядку sentence:
-
-	// std::string sentence = "The quick brown fox jumps over the lazy dog.";
-    // int unique_word_count = count_unique_words(sentence);
-
-    // std::cout << "There are " << unique_word_count << " unique words in the sentence." << std::endl;
-
-    return 0;
-}
-
-bool is_anagram(std::string word, std::string anagram) {
-    std::set<char> word_set(word.begin(), word.end());
-    std::set<char> anagram_set(anagram.begin(), anagram.end());
-
-    return word_set == anagram_set;
-}
-
-int count_unique_words(std::string sentence) {
-    std::set<std::string> unique_words;
-    std::stringstream ss(sentence);
-    std::string word;
-
-    while (ss >> word) {
-        unique_words.insert(word);
+    std::cout << "\nНовий map після insert: " << std::endl;
+    auto it = mp.begin();
+    for (int i = 0; it != mp.end(); ++it, ++i) {  // виводимо їх
+        std::cout << i << ") Ключ " << it->first << ", значення " << it->second << std::endl;
     }
 
-    return unique_words.size();
+    mp[ 0 ] = 9;
+
+    // std::cout << "\nНовий map після додавання елемента: " << std::endl;
+    it = mp.begin();
+    for (int i = 0; it != mp.end(); ++it, ++i) {  // виводимо їх
+        std::cout << i << ") Ключ " << it->first << ", значення " << it->second << std::endl;
+    }
+
+    // Приклад використання find
+    // std::map<std::string, std::string> book_example;  
+    // book_example["book"] = "книга";     
+
+    // std::map<std::string, std::string>::iterator it_book;
+
+    // it_book = book_example.find("book");
+
+    // if ( it_book != book_example.end() ) {
+    //     std::cout << it_book->second << std::endl;
+    // } else {
+    //     std::cout << "Ключа зі значенням 'book' немає" << std::endl;
+    // }
+
+    // it_book = book_example.find("books");
+    // if ( it_book == book_example.end() ) {
+    //     std::cout << "Ключа зі значенням 'books' немає" << std::endl;
+    // }
+
+
+    // // Приклад використання erase
+    std::map<std::string, std::string> passport;
+    passport["maxim"] = "Denisov";     // додаємо нові значення
+    passport["andrey"] = "Puzerevsky";
+    passport["dima"] = "Tilyupo";
+    
+    std::map<std::string, std::string>::iterator full_name; // створили ітератор на passport
+    
+    full_name = passport.begin();
+    for (int i = 0; full_name != passport.end(); ++full_name, ++i) {  // виводимо їх
+        std::cout << i << ") Ключ " << full_name->first << ", значення " << full_name->second << std::endl;
+    }
+    std::cout << "Розмір map: " << passport.size() << std::endl;
+
+    std::string str;
+    std::cout << "Введіть ключ для видалення: ";
+    std::cin >> str;
+    full_name = passport.find(str); // знаходимо елемент
+    if (full_name != passport.end()) {
+        passport.erase(full_name); // видаляємо елемент
+    } else {
+        std::cout << "Елемент з ключем " << str << " не знайдений" << std::endl;
+    }
+     
+    full_name = passport.begin();
+    for (int i = 0; full_name != passport.end(); ++full_name, ++i) {  // виводимо їх
+        std::cout << i << ") Ключ " << full_name->first << ", значення " << full_name->second << std::endl;
+    }
+    std::cout << "Розмір map після видалення: " << passport.size() << std::endl;
+
+    return 0;
 }
