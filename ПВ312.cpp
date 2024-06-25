d427bb32a55725d1fd666cc5ae0594b8e8281a9c
Khorishchenko <khorischenkosergey@gmail.com>
2024-06-25 16:49:59 +0300

    file change

--- ПВ312.cpp
+++ ПВ312.cpp
@@ -30,160 +30,296 @@
 #include <sstream>
 
 
-
-// https://www.youtube.com/watch?v=1cKvMZOJeeE&ab_channel=%23SimpleCode - VECTOR
-
-#include <map>
+#include <iostream>
+#include <chrono>
+#include <list>
 #include <iterator>
+#include <algorithm> // для функції std::max_element
+#include <map>
+#include <set>
+#include <sstream>
+#include <queue>
+
+// https://youtu.be/bP5GJN3-zIo
+
 using namespace std;
 
-// https://codelessons.ru/cplusplus/map-v-c-chto-eto-i-kak-s-etim-rabotat.html --- MAP
-#include <iostream>
-#include <map>
-#include <string>
 
-int main() {
-    // Як створити map
-    // map <L, R> <ім'я>;
-    // <L> — цей тип даних буде відноситись до значення ключа.
-    // <R> — цей тип даних відноситься до значення.
-
-    // Приклад 1: map з ключами типу int і значеннями типу int
-    std::map < int, int > mp1;
-
-    mp1[ 0 ] = 7; // ключ - число
-    std::cout << "mp1[0] = " << mp1[0] << std::endl;
-
-    // Приклад 2: map з ключами типу string і значеннями типу int
-    std::map<std::string, int> mp2;
-    mp2["zero"] = 4;  // ключ - рядок
-    mp2["zero"] = 10;
-    std::cout << "mp2[\"zero\"] = " << mp2["zero"] << std::endl;
-
-    // Приклад 3: map з ініціалізацією елементів
+//  Предикат, Функтор, Лямбда Функції
 
 
 
+// Функтор
 
-    // std::map<std::string, std::string> book {
-    //     {"Hi", "Привет"},
-    //     {"Student", "Студент"},
-    //     {"!", "!"}
+// Функтор (Functor): Функтор - це об'єкт, який перевантажує оператор додавання operator(). 
+// Він дозволяє об'єкту вести себе як функція і приймати аргументи, подібно до функції.
+
+
+
+class Student
+{
+public:
+    Student(std::string firstName, std::string lastName, int id) :
+        m_firstName(firstName), m_lastName(lastName), m_id(id) {}
+
+    std::string GetFirstName() const { return m_firstName; }
+    std::string GetLastName() const { return m_lastName; }
+    int GetId() const { return m_id; }
+
+private:
+    std::string m_firstName;
+    std::string m_lastName;
+    int m_id;
+};
+
+
+
+
+
+class StudentSorter
+{
+public:
+    bool operator() (const Student& s1, const Student& s2)
+    {
+        return (s1.GetFirstName() < s2.GetFirstName());
+    }
+};
+
+
+
+class MyFunctor
+{
+    public:
+        void operator()()
+        {
+            std::cout << "i am functor " << std::endl;
+        }
+
+};
+
+
+int main()
+{
+    MyFunctor f;
+
+    // f();         // Як виклик звичайної функції!
+
+
+
+
+    // std::list<Student> students = {
+    //     Student("John", "Doe", 1),
+    //     Student("Jane", "Smith", 2),
+    //     Student("Bob", "Johnson", 3),
+    //     Student("Alice", "Williams", 4),
+    //     Student("Dave", "Brown", 5)
     // };
 
-    // std::cout << "book[\"Hi\"] = " << book["Hi"] << std::endl;
-    // book["Hi"] = "Hello";                       
-    // std::cout << "book[\"Hi\"] = " << book["Hi"] << std::endl;
+    // students.sort(StudentSorter());
 
-
-
-
-
-
-
-
-
-    // Використання ітераторів для map
-    // map <тип даних>::iterator <ім'я>;
-
-    // Створення map для прикладу з ітераторами
-
-
-
-    std::map<int, int> mp;
-    // std::cout << "Введіть кількість елементів: "; 
-    // int n; 
-    // std::cin >> n;
- 
-    // for (int i = 0; i < n; i++) {
-    //     std::cout << "Елемент " << i + 1 << ": "; 
-    //     int a, b; 
-    //     std::cin >> a >> b;
-    //     mp[a] = b;  // додаємо нові елементи
-    // }
- 
-    // std::map<int, int>::iterator it = mp.begin();
-
-    // // auto it_auto = mp.begin();
-    // // auto value = 10;
-
-    // std::cout << "Всі елементи map, відсортовані за ключем: " << std::endl;
-    // for (int i = 0; it != mp.end(); ++it, ++i) {  // виводимо їх
-    //     std::cout << i << ") Ключ " << it->first << ", значення " << it->second << std::endl;
+    // for (const auto& student : students)
+    // {
+    //     std::cout << student.GetLastName() << ", " << student.GetFirstName() << " (" << student.GetId() << ")" << std::endl;
     // }
 
-    // for ( const auto& element : mp )
-    //     std::cout <<  " Ключ " << element.first << ", значення " << element.second << std::endl;
-
-
-    // Приклад використання insert
-    int key = 0, value = 5;
-    mp[1] = 11;
-    mp.insert(std::make_pair(key, value));
-
-    std::cout << "\nНовий map після insert: " << std::endl;
-    auto it = mp.begin();
-    for (int i = 0; it != mp.end(); ++it, ++i) {  // виводимо їх
-        std::cout << i << ") Ключ " << it->first << ", значення " << it->second << std::endl;
-    }
-
-    mp[ 0 ] = 9;
-
-    // std::cout << "\nНовий map після додавання елемента: " << std::endl;
-    it = mp.begin();
-    for (int i = 0; it != mp.end(); ++it, ++i) {  // виводимо їх
-        std::cout << i << ") Ключ " << it->first << ", значення " << it->second << std::endl;
-    }
-
-    // Приклад використання find
-    // std::map<std::string, std::string> book_example;  
-    // book_example["book"] = "книга";     
-
-    // std::map<std::string, std::string>::iterator it_book;
-
-    // it_book = book_example.find("book");
-
-    // if ( it_book != book_example.end() ) {
-    //     std::cout << it_book->second << std::endl;
-    // } else {
-    //     std::cout << "Ключа зі значенням 'book' немає" << std::endl;
-    // }
-
-    // it_book = book_example.find("books");
-    // if ( it_book == book_example.end() ) {
-    //     std::cout << "Ключа зі значенням 'books' немає" << std::endl;
-    // }
-
-
-    // // Приклад використання erase
-    std::map<std::string, std::string> passport;
-    passport["maxim"] = "Denisov";     // додаємо нові значення
-    passport["andrey"] = "Puzerevsky";
-    passport["dima"] = "Tilyupo";
-    
-    std::map<std::string, std::string>::iterator full_name; // створили ітератор на passport
-    
-    full_name = passport.begin();
-    for (int i = 0; full_name != passport.end(); ++full_name, ++i) {  // виводимо їх
-        std::cout << i << ") Ключ " << full_name->first << ", значення " << full_name->second << std::endl;
-    }
-    std::cout << "Розмір map: " << passport.size() << std::endl;
-
-    std::string str;
-    std::cout << "Введіть ключ для видалення: ";
-    std::cin >> str;
-    full_name = passport.find(str); // знаходимо елемент
-    if (full_name != passport.end()) {
-        passport.erase(full_name); // видаляємо елемент
-    } else {
-        std::cout << "Елемент з ключем " << str << " не знайдений" << std::endl;
-    }
-     
-    full_name = passport.begin();
-    for (int i = 0; full_name != passport.end(); ++full_name, ++i) {  // виводимо їх
-        std::cout << i << ") Ключ " << full_name->first << ", значення " << full_name->second << std::endl;
-    }
-    std::cout << "Розмір map після видалення: " << passport.size() << std::endl;
 
     return 0;
 }
+
+
+
+
+
+
+
+// Предикати с++
+
+// Придикат (Predicate): Придикат - це функція або об'єкт, який приймає один або декілька аргументів і 
+// повертає логічне значення (зазвичай true або false). Він використовується для визначення, 
+// чи виконується певна умова. Придикати часто використовуються в функціях фільтрації, сортування або пошуку даних.
+using namespace std;
+
+bool f(int16_t a){
+   return a < 0;
+}
+
+bool compareLength(const std::string& a, const std::string& b) {
+    return a.length() < b.length();
+}
+
+bool isOdd(int x) {
+    return x % 2 != 0;
+}
+
+bool isEqual(int x, int y) {
+    return x == y;
+}
+
+
+
+// int main() 
+// {
+
+
+//     vector<int> v {-1, -2, 4, 3, 0, -3, -9};
+
+//     // // Предикаты с++
+//     cout << count_if(v.begin(), v.end(), f) << endl;
+
+
+     
+// //  сортування вектора за довжиною рядків, використовуючи предикат
+//     std::vector<std::string> vec = {"apple", "banana", "kiwi", "orange"};
+
+  
+//     std::sort(vec.begin(), vec.end(), compareLength);
+
+//     for (const auto& str : vec) {
+//         std::cout << str << " ";
+//     }
+//     std::cout << std::endl;
+
+
+
+// // Використання предиката для видалення елементів зі списку, що задовольняють певну умову:
+//   std::list<int> l = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+//     l.remove_if(isOdd);
+
+//     for (const auto& x : l) {
+//         std::cout << x << ' ';
+//     }
+//     std::cout << '\n';
+
+
+// // Використання предиката для порівняння елементів в двох масивах:
+//     int arr1[] = {1, 2, 3, 4, 5};
+//     int arr2[] = {1, 2, 3, 4, 5};
+
+//     bool result = std::equal(std::begin(arr1), std::end(arr1), std::begin(arr2), isEqual);
+
+//     std::cout << (result ? "Equal" : "Not equal") << '\n';
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://youtu.be/bP5GJN3-zIo
+
+using namespace std;
+
+
+// void process_data(const std::vector<int>& data, std::function<void(int)> func) {
+//     for (auto d : data) {
+//         func(d);
+//     }
+// }
+
+
+
+// class Person {
+// public:
+//     Person(std::string name, int age) : name_(name), age_(age) {}
+
+//     std::string get_name() const { return name_; }
+//     int get_age() const { return age_; }
+
+// private:
+//     std::string name_;
+//     int age_;
+// };
+
+
+// void print_vector(std::vector<Person> &arr, std::function<void(Person)> fun)
+// {
+// 	for (auto &element : arr)
+// 		fun(element);
+// }
+
+
+// int main()
+// {
+
+	// Лямбда-вираз у С++ — анонімна функція, яка підтримує стан між викликами і 
+	// може отримати доступ до змінних зі своєї області видимості. Використовуючи лямбда-вирази, можна оголошувати функції в будь-якому місці коду.
+
+
+	// std::vector<int> arr{1, 2, 3, 4, 5};
+
+	// auto f = [](int a) 
+	// {
+	// 	std::cout << "Element vector is: " << a << std::endl;
+
+	// };
+
+	// print_vector(arr, [](int a) 
+	// {
+	// 	std::cout << "Element vector is: " << a << std::endl;
+
+	// });
+
+
+	// auto sum = [](int a, int b) { return a + b; };
+	// int result = sum(2, 3); // result буде 5
+
+	// std::cout << result << std::endl;
+
+
+	// Використання лямбда-функції в якості параметру функції:
+
+	// std::vector<int> vec = {3, 2, 1, 5, 4};
+	// process_data(vec, [](int val) { std::cout << val << " "; });
+
+
+
+	// Використання лямбда-функції для порівняння об'єктів класу:
+	// Цей код відсортує вектор об'єктів класу Person за зростанням віку.
+
+	// std::vector<Person> people = {
+	// 	{"Alice", 25},
+	// 	{"Bob", 30},
+	// 	{"Charlie", 20},
+	// };
+
+
+// 	std::function<void(Person)> fun = [](const Person &a)
+// 	{
+// 		std::cout << a.get_age() << ' ' << a.get_name() << std::endl;
+// 	};
+
+// 	print_vector(people, fun);
+
+// 	std::sort(people.begin(), people.end(), [](const Person& a, const Person& b) {
+// 		return a.get_age() < b.get_age();
+// 	});
+
+// 	print_vector(people, fun);
+
+
+
+
+//     return 0;
+// }
+
+
+
