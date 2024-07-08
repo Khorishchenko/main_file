78dd23258daf251f999347bbf27e6b2523036c08
Khorishchenko <khorischenkosergey@gmail.com>
2024-07-08 12:12:53 +0300

    changes file

--- ПВ421.cpp
+++ ПВ421.cpp
@@ -27,262 +27,53 @@ using namespace std;
 
 
 
-// Повторення.
-
-// Базові поняття ООП:
-
-// Класи та об'єкти
-// Інкапсуляція
-// Наслідування
-// Поліморфізм
-// Абстракція
 
 
 
+// // Не можна перегрузити !
 
-// Класи і об'єкти:
-// - Визначення класів
-// - Конструктори та деструктори
-// - Методи та атрибути
-// - Статичні члени класу
-// - Доступ до приватних даних через методи
+// // .   
+// // * 
+// // :: 
+// // ?:
+// // #
+// // sizeof
 
 
+class Point
+{
 
-// class Human
-// {
-// private:
-//     int age;
-//     std::string name;
-
-//     const int id;
-
-// public:
-
-//     // Human()
-//     // {
-//     //     age = 0;
-//     //     name = "no name";
-//     // }
-
-//     // Human(std::string name, int age) : name(name), age(age)
-//     // {
-        
-//     // }
-
-//     Human( std::string Name = "no name", int Age  = 0 , int Id = 0) : name( Name ) , age( Age ), id( Id )
-//     {
-        
-//     }
-
-//     int getAge()
-//     {
-//         return age;
-//     }
-
-//     std::string getName()
-//     {
-//         return name;
-//     }
-
-//     void setAge( int age )
-//     {
-//         this->age = age;
-//     }
-
-//     void setName(std::string name)
-//     {
-//         this->name = name;
-//     }
-
-// };
-
-// int main()
-// {
-//     Human people("Anton", 24, 5);
-
-//     // people.setAge(20);
-//     // people.setName("Anton");
-
-//     std::cout << "Name: " << people.getName() << ' ' << "Age: " << people.getAge() << std::endl;
-
-// }
- 
+};
 
 
 
 
 
+// Перевантажимо оператори порівняння >, <, >= та <= == and !=
 
 
-// =============================================//
-// Деструктор
+class Sum{
+    int a;
+    public:
+        Sum(int a) : a (a) {}
 
-
-// class Array
-// {
-// private:
-//     int *ptr;
-//     int size;
-// public:
-
-//     Array ( int size = 0 )
-//     {
-//         if ( size > 0 )
-//         {
-//             ptr = new int[size];
-
-//             for ( int i = 0; i < size; i++)
-//                 ptr[i] = rand() % 10;
-
-//             this->size = size;
-//         }
-//         else
-//         {
-//             ptr = nullptr;
-//             size = 0;
-//         }
-//     }
-
-//     void Print()
-//     {
-//         if ( ptr != nullptr )
-//         {
-//             for ( int i = 0; i < size; i++ )
-//                 std::cout << ptr[i] << ' ';
-//             std::cout << "\n";
-//         }
-//         else
-//         {
-//             std::cout << "our arr is empty " << std::endl;
-//         }
-//     }
-
-//     ~Array()
-//     {
-//         if ( ptr != nullptr )
-//         {
-//             delete[] ptr;
-//             std::cout << " destructor delete arr " << std::endl;
-//         }
-//         else
-//         {
-//              std::cout << " destructor " << std::endl;
-//         }
-
-//     }
-// };
+};
 
 
 // int main()
 // {
-//     Array arr;
-//     arr.Print();
-
-//     Array arr1( 15 );
-//     arr1.Print();
-
-//     {
-
-//         Array o1( 10 );
-//         o1.Print();
+//     Sum a(30);
+//     Sum b(30);
 
 
-//     }
-
-//     std::cout << "hello " << std::endl;
+//     if(a > b)
+//         std::cout << "a > b" << std::endl;
+//     else if(a == b)
+//         std::cout << "a == b" << std::endl;
+//     else
+//         std::cout << " a < b" << std::endl;
 
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// =============================================//
-// Порядок виклику конструкторів та деструкторів
-
-// class A{
-// public:
-//     A() {std::cout << "This is const A" << std::endl;}
-//     ~A() { std::cout << "This is dest A " << std::endl; }
-// };
-
-
-// class B{
-// public:
-//     B() {std::cout << "This is const B" << std::endl;}
-//     ~B() { std::cout << "This is dest B " << std::endl; }
-// };
-
-// class C{
-// public:
-//     C() {std::cout << "This is const C" << std::endl;}
-//     ~C() { std::cout << "This is dest C" << std::endl; }
-// };
-
-// int main()
-// {
-//     A a;
-//     B b;
-
-//     // {
-//     //     A a1;
-//     // }
-
-//     C c;
-// }
-
-
-
-
-
-
-//=================================================================//
-// Реалізація методів за межами класу
-// Спискова ініціалізація
-
-
-// class Titan
-// {
-//     std::string _name;
-//     int m_age;
-//     const int size;
-// public:
-//     Titan(std::string name, int conssst) : _name(name), m_age(0), size(conssst)
-//     {
-//         // size = conssst;
-//     }
-
-//     void showName_Titan();
-// };
-
-// void Titan::showName_Titan()
-// {
-//     std::cout << _name << ' ' << m_age << ' '  << size << std::endl;
-// }
-
-// int main(){
-//     Titan b("Mark", 10);
-   
-//     b.showName_Titan();
-// }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -290,37 +81,56 @@ using namespace std;
 
 
  
-
-// THIS*
-
-// class MyClass {
-// public:
-//     void SetData( int Data ) {
-//         data = Data;  // Використовуємо this для доступу до змінної-члена data
-//     }
-
-//     int GetData() {
-//         return this->data;  // Використовуємо this для отримання значення data
-//     }
-
-//     void GetPtr()
-//     {
-//         std::cout << this << std::endl;
-//     }
-
-// private:
-//     int data;
-// };
-
-// int main() {
-//     MyClass obj;
-//     obj.SetData(42);  // Встановлюємо значення data за допомогою методу SetData
-
-//     obj.GetPtr();
+// Перевантаження операторів інкременту та декременту
+// Оскільки оператори інкременту та декременту є унарними та змінюють свої операнди,
+// то навантаження слід виконувати через методи класу.
 
 
+class Number
+{
+    int a;
+    public:
+    Number(int a ) : a(a){}
 
-//     std::cout << "Data: " << obj.GetData() << std::endl;  // Отримуємо значення data за допомогою методу GetData
+
+    Number operator++(){
+        ++(this->a);
+        return *this;
+    }
+
+    Number operator++(int){
+         Number tmp(this->a);
+        ++(this->a);
+        return tmp;
+    }
+
+
+    friend std::ostream& operator<<(std::ostream &out, const Number &obj);
+};
+
+std::ostream& operator<<(std::ostream &out, const Number &obj)
+{
+    out << obj.a;
+    return out;
+}
+
+// int main()
+// {
+//     Number a(10);
+
+
+
+//     std::cout << ++a << std::endl;
+//     std::cout << a++ << std::endl;
+//     std::cout << a << std::endl;
+
+
+
+//     // int x = 10;
+
+//     // std::cout << ++x << std::endl;
+//     // std::cout << x++ <<  ' ' << x << std::endl;
+//     // std::cout << x   <<   std::endl;
 
 //     return 0;
 // }
@@ -328,90 +138,130 @@ using namespace std;
 
 
 
+// Перегрузка оператора индексации []
 
-
-
-
-
-
-
-// https://youtu.be/Ic19I0kcBnU - Дружні функції та класи приклад
-
-// Дружні функції
-
-class Point
+class Int
 {
+public:
+    Int()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            arr[i] = rand() % 10;
+        }
+    }
+
+    int getArray(int index)
+    {
+        return arr[index];
+    }
+
+    void SetArray(int index, int value)
+    {
+        arr[index] = value;
+    }
+
+    int& operator[](int index)
+    {
+        return arr[index];
+    }
+
+
+    void Print() const{
+        for (int i = 0; i < 10; i++)
+        {
+            std::cout << arr[i] << ' ';
+        }
+        std::cout << std::endl;
+    }
+
 private:
-    int x;
-    int y;
-public:
-
-    Point(int x, int y) : x(x), y(y) {}
-
-    friend void print( Point obj );
-};
-
-void print( Point obj )
-{
-    std::cout << obj.x << ' ' << obj.y << std::endl;
-}
-
-int main()
-{
-    Point o1(10, 20), o2(30, 40);
-
-    print( o1 );
-    print( o2 );
-}
-
-
-
-
-class B;
-
-class A
-{
-    int valueA;
-public:
-    A ( int value ) : valueA(value) {}
-
-    friend void sum ( A objA, B objB );
-};
-
-class B
-{
-    int valueB;
-public:
-    B( int value ) : valueB(value) {}
-
-    friend void sum ( A objA, B objB );
+    int arr[10];    
 
 };
 
-void sum(A, B);
 
-int main()
+
+// int main()
+// {
+
+//         Int o1;
+//         o1.Print();
+
+// // початок
+
+    // int arr[10];
+
+    // arr[5] = 6;
+    // std::cout << arr[5] << std::endl;
+
+// // Можна використовувати геттери і сеттери:
+
+
+    // std::cout << o1.getArray(4) << std::endl;
+
+    // o1.SetArray(4, 10);
+
+    // std::cout << o1.getArray(4) << std::endl;
+
+
+
+    // o1[4] = 20;
+    // std::cout << o1[4] << std::endl;
+
+
+
+
+
+// // Можна також просто повертати весь масив (array) і використовувати оператор [] для доступу до його елементів:
+
+//     Int a;
+  
+//     a.getArr()[4] = 6;
+//     std::cout << a.getArray(4) << std::endl;
+
+
+// // Але можна зробити ще простіше, перевантаживши оператор індексації.
+//     a[4] = 7;
+
+//     std::cout << a[4] << std::endl;
+//     return 0;
+// }
+
+
+
+
+//  Перегрузка оператора ()
+
+
+class Operaor
 {
-    A o1(10);
-    B o2(20);
-    sum( o1, o2 );
-    sum( A(20), B(30) );
-
-
-}
-
-void sum ( A objA, B objB )
-{
-    std::cout << objA.valueA + objB.valueB << std::endl;
-}
+    private:
+        int arr[5][5];
+    public:
+     
+        friend std::ostream& operator<<(std::ostream &out, Operaor &o);
+};
 
 
 
+// int main()
+// {
+//     Operaor a;
+
+//     std::cout << a << std::endl;
+
+//     a(3, 3) = 5;
+
+//     std::cout << a << std::endl;
 
 
-// Умова завдання 2:
+//     a();
 
-// Створіть клас Person, який представляє особу з ім'ям і віком. Клас повинен мати два конструктори: один, який приймає ім'я та вік, та інший, 
-// який приймає лише ім'я. 
-// В цьому випадку вік має ініціюватися значенням за замовчуванням (наприклад, 0). 
-// Використовуйте делегування конструкторів для забезпечення можливості створювати об'єкти Person з ім'ям і віком або лише з ім'ям.
+//     std::cout << a << std::endl;
+
+//     a(10);
+
+//     std::cout << a << std::endl;
+
+// }
