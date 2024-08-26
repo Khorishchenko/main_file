05845027d0064adff4e33b4f857e5d2bae69a8a4
Khorishchenko <khorischenkosergey@gmail.com>
2024-08-26 13:31:07 +0300

    changes/add files

--- main.cpp
+++ main.cpp
@@ -9,35 +9,59 @@
 #include <queue>
 #include <vector>
 using namespace std;
-
+// chcp 65001
+#include <iostream>
+#include <set>
+#include <sstream>
+#include <string>
+using namespace std;
+  
+using namespace std;
+  
+using namespace std;
  
-class A {
-    int a;
- 
-public:
-    A(int x) {
-        a = x;
-        cout << "A(" << a << ")\n";
-    }
- 
-    ~A() {
-        cout << "~A(" << a << ")\n";
-    }
-
-    friend void operator << (std::ostream &out, const A &o1);
-};
- 
-A f() {
-    A a1(123);
-    return a1;
-}
-
-void operator << (std::ostream &out, const A& o1)
+vector<int> Arrays(const vector<int>& arr1, const vector<int>& arr2)
 {
-    out << o1.a;
+    vector<int> togetherArray;
+    int i = 0, j = 0;
+ 
+    while (i < arr1.size() && j < arr2.size())
+    {
+        if (arr1[i] <= arr2[j])
+        {
+            togetherArray.push_back(arr1[i]);
+            i++;
+        }
+        else
+        {
+            togetherArray.push_back(arr2[j]);
+            j++;
+        }
+    }
+ 
+    while (i < arr1.size())
+    {
+        togetherArray.push_back(arr1[i]);
+        i++;
+    }
+    while (j < arr2.size())
+    {
+        togetherArray.push_back(arr2[j]);
+        j++;
+    }
+ 
+    return togetherArray;
 }
  
-int main() {
-    std::cout << f();
-    return 0;
+int main()
+{
+    vector<int> arr1 = { 1, 3, 5, 7 };
+    vector<int> arr2 = { 2, 4, 6, 8 };
+ 
+    vector<int> result = Arrays(arr1, arr2);
+ 
+    cout << "Together Array: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
 }
