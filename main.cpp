4cacbd986cab8cd84e698b79d11eebcd710d5734
admin <admin@MacBook-Air-admin.local>
2025-02-15 00:43:59 +0200

    uo

--- main.cpp
+++ main.cpp
@@ -12,36 +12,40 @@
 #include <cassert>
 
 
+// Таким чином, завдяки цій функції ви можете встановити місце розташування як Україна або інша країна і користуватися тим алфавітом і тим часом, який є в цій країні.
+// #include "windows.h"
+// SetConsoleCP(65001); //UTF-8
+// SetConsoleOutputCP(65001); //UTF-8
 
 
 
 // 2. Поглиблене вивчення функцій:
 // Різновиди функцій:
-// Функції-члени класів (методи).
-// Шаблони функцій (template).
-// Лямбда-функції.
-// Передача аргументів:
-// За значенням, за вказівником, за посиланням.
-// Ключове слово const у параметрах функцій.
-// Перевантаження функцій та операторів.
+// // Функції-члени класів (методи).
+// // Шаблони функцій (template).
+// // Лямбда-функції.
+// // Передача аргументів:
+// // За значенням, за вказівником, за посиланням.
+// // Ключове слово const у параметрах функцій.
+// // Перевантаження функцій та операторів.
 
 
 
-// 3. Управління пам’яттю:
-// Статична та динамічна пам’ять:
-// Що таке стек і купа (stack and heap).
-// Використання оператора new та delete.
-// Робота з покажчиками:
-// Що таке покажчик, арифметика покажчиків.
-// Покажчики на функції.
-// Розумні вказівники (smart pointers):
-// unique_ptr, shared_ptr, weak_ptr.
-// Уникнення витоків пам’яті (memory leaks).
+// // 3. Управління пам’яттю:
+// // Статична та динамічна пам’ять:
+// // Що таке стек і купа (stack and heap).
+// // Використання оператора new та delete.
+// // Робота з покажчиками:
+// // Що таке покажчик, арифметика покажчиків.
+// // Покажчики на функції.
+// // Розумні вказівники (smart pointers):
+// // unique_ptr, shared_ptr, weak_ptr.
+// // Уникнення витоків пам’яті (memory leaks).
 
 
 
-int AddTwo(int);
-void print(int);
+// int AddTwo(int);
+// void print(int);
 
 
 int add(int a, int b) {
@@ -52,114 +56,19 @@ int subtract(int a, int b) {
     return a - b;
 }
 
-template <typename T, typename T1>
-void Print(const T value, const T1 valueTwo)
-{
-    std::cout << value << ' ' << valueTwo << std::endl;
-}
+// template <typename T, typename T1>
+// void Print(const T value, const T1 valueTwo)
+// {
+//     std::cout << value << ' ' << valueTwo << std::endl;
+// }
 
-template <typename T>
-void Swap(T& , T& );
+// template <typename T>
+// void Swap(T& , T& );
 
 
 int main()
 {
-    Print(10, 'A');
-    Print("Hello", 10);
-    Print(12.34, 12);
-    Print(true, "Cat");
-    Print('A', 2.3);
 
-
-
-    std::string str1 = " Cat", str2 = "Dog";
-    Swap(str1, str2);
-    std::cout << "str1 is: " << str1 << ", str2 is: " << str2 << std::endl;
-
-    int x = 10, y = 5;
-    Swap(x, y);
-    std::cout << "x is: " << x << ", y is: " << y << std::endl;
-
-
-
-
-    // [] () {};
-
-    auto PRINT = [] (int x, int y) 
-    {
-        std::cout << "x is: " << x << ", y is: " << y << std::endl;
-    };
-
-    PRINT(10, 5);
-
-
-    int arr[5] {4, 2, 0, 8, 10};
-    std::sort(std::begin(arr), std::end(arr), [](const int x, const int y){
-        return x > y;  
-    });
-
-    for (int i = 0; i < 5; i++)
-    {
-        std::cout << arr[i] <<' ';
-    }
-
-    bool arr_positiv= std::all_of(std::begin(arr), std::end(arr), [](const int y){
-        return y > 0;  
-    });
-
-    if (arr_positiv)
-    {
-        std::cout << "All elements in array are positiv." << std::endl;
-    }
-    else 
-    {
-        std::cout << "Not all elements in array are positiv." << std::endl;
-    }
-
-
-    auto mulTwo = [](int x)
-    {
-        return x * 2;
-    };
-
-    std::cout << mulTwo(5) << std::endl;
-
-
-
-    int value = 2;
-
-    auto mulTwo = [ &value ](int y)
-    {
-        // std::cout << value << std::endl;
-        
-        // y = value * y;
-        // std::cout << y << std::endl;
-
-        value = y * 2;
-
-    };
-
-    mulTwo(5);
-    std::cout << "Value is: " << value << std::endl;
-
-    // [] () {};
-
-
-
-
-
-
-
-
-
-
-
-
-    // [](int *ptr) {
-       
-    //    ptr[]
-    // }
- 
 
      // Показник на ціле число:
     int number = 42;
@@ -291,22 +200,22 @@ int main()
     return 0;   
 }
 
-template <typename T>
-void Swap(T& value, T& valueTwo)
-{
-    T temp = value;
-    value = valueTwo;
-    valueTwo = temp;
-}
+// template <typename T>
+// void Swap(T& value, T& valueTwo)
+// {
+//     T temp = value;
+//     value = valueTwo;
+//     valueTwo = temp;
+// }
 
 
-void print(int number)
-{
-    std::cout << "number is " << number << std::endl;
-}
+// void print(int number)
+// {
+//     std::cout << "number is " << number << std::endl;
+// }
 
 
-int AddTwo(int number)
-{
-    return number + 2;
-}
+// int AddTwo(int number)
+// {
+//     return number + 2;
+// }
